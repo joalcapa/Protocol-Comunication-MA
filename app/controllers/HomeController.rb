cf348eb046bdfe59afed36be5760f612961cb155
joalcapa <josecaceres.oreul@gmail.com>
2017-04-09 19:22:55 -0430

    Threads

--- app/controllers/HomeController.rb
+++ app/controllers/HomeController.rb
@@ -1,7 +1,19 @@
 class HomeController
     
     def initialize(homeModel, homeView)
-        @homeModel, @homeView = homeModel, homeView
+        @homeModel, @homeView, @running = homeModel, homeView, true
+        @thread = Thread.new{ run() }
+    end
+    
+    def run()
+        while(@running)
+            puts "hola"
+            sleep(1);
+        end
+    end
+    
+    def threadPresent()
+        return @thread
     end
     
 end
--- app/views/HomeView.rb
+++ app/views/HomeView.rb
@@ -3,12 +3,20 @@ require 'tk'
 class HomeView
     
     def initialize()
+        @thread = Thread.new{ run() }
+    end
+    
+    def run()
         root = TkRoot.new { title "ProtocolRuby" }
         TkLabel.new(root) do
             text 'ProtocolRuby'
             pack { padx 15 ; pady 15; side 'left' }
         end
         Tk.mainloop
+    end
+    
+    def threadPresent()
+        return @thread
     end
     
 end
