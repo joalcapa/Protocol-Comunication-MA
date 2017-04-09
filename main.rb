f72e592dc26297030eb1c76857c1fc484593ea83
Jose Caceres <josecaceres.oreul@gmail.com>
2017-04-09 18:14:09 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ load 'app/controllers/HomeController.rb'
 load 'app/models/HomeModel.rb'
 load 'app/views/HomeView.rb'
 
+# Initialization of the application based on MVC (Model - View - Controller)
 homeModel = HomeModel.new()
 homeView = HomeView.new()
 homeController = HomeController.new(homeModel, homeView)
