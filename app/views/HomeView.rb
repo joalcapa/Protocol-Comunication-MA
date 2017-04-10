3d7ed80f3d0534360510665faeeb1dd9816db1e6
joalcapa <josecaceres.oreul@gmail.com>
2017-04-10 10:50:50 -0430

    Apply GUI

--- app/views/HomeView.rb
+++ app/views/HomeView.rb
@@ -1,34 +1,89 @@
 require 'tk'
+require 'tkextlib/tile'
 
 class HomeView
     
-    def initialize()
-        config() 
+    STR_MODE_CLIENT = 'MODE CLIENT'
+    STR_MODE_SERVER = 'MODE SERVER' 
+    STR_LOOKING_SERVER = 'Looking server ...'
+    
+    LOOKING_SERVER = 1
+    
+    def initialize
+        @statusServer = false
+        @statusOperationClient = LOOKING_SERVER
+        initContext()
+        changeMode()
         @thread = Thread.new{ run() }
     end
     
-    def config() 
-        if (ENV['TYPE_SERVICE'] == 'client') then
+    def initContext
+        root = TkRoot.new {title "Feet to Meters"}
+        content = Tk::Tile::Frame.new(root) {
+            padding "3 3 12 12"
+            }.grid( :sticky => 'nsew')
+        
+        TkGrid.columnconfigure root, 0, :weight => 1; TkGrid.rowconfigure root, 0, :weight => 1
+        
+        $feet = TkVariable.new; $meters = TkVariable.new
+        f = Tk::Tile::Entry.new(content) {
+            width 7;
+            textvariable $feet
+            }.grid( :column => 2, :row => 1, :sticky => 'we')
+        Tk::Tile::Label.new(content) {
+            textvariable $meters
+            }.grid( :column => 2, :row => 2, :sticky => 'we')
+        
+        @modeBtn = TkButton.new(
+            content,
+            :text => '',
+            :command => proc {changeMode}
+                ).pack.grid( :column => 1, :row => 3, :sticky => 'w')
+        
+        @modeTxt = Tk::Tile::Label.new(content) {
+            text ''
+            }.grid( :column => 1, :row => 1, :sticky => 'w')
+            
+        @messageTxt = Tk::Tile::Label.new(content) {
+            text ''
+            }.grid( :column => 3, :row => 3, :sticky => 'w')
+        
+        TkWinfo.children(content).each {|w| TkGrid.configure w, :padx => 5, :pady => 5}
+        f.focus
+        root.bind("Return") {changeMode}
+    end
+        
+    def changeMode
+        if (@statusServer) then
+            @modeBtn.text = STR_MODE_CLIENT
+            @modeTxt.text = STR_MODE_SERVER
             @statusServer = false
         else
+            @modeBtn.text = STR_MODE_SERVER
+            @modeTxt.text = STR_MODE_CLIENT
             @statusServer = true
+        end
+        changeMessage()
+    end
+        
+    def changeMessage
+        case @statusOperationClient
+            when LOOKING_SERVER
+                @messageTxt.text = STR_LOOKING_SERVER
+            else
+                @messageTxt.text = STR_LOOKING_SERVER
         end
     end
         
-    def run()
-        root = TkRoot.new { title "ProtocolRuby" }
-        TkLabel.new(root) do
-            text 'ProtocolRuby'
-            pack { padx 15 ; pady 15; side 'left' }
-        end
+    def run
         Tk.mainloop
     end
     
-    def threadPresent()
+    def threadPresent
         return @thread
     end
     
-    def statusServer()
+    def statusServer
         return @statusServer
     end
     
