b2a9fbc2aacb556585dc26e16ef98cb48a1597c7
joalcapa <josecaceres.oreul@gmail.com>
2017-04-09 21:27:06 -0430

    Revert "Revert "Implementation of UDPSockets, for the client""
    This reverts commit 95bc511ebe107aaa144df6990a479a4fb3a8c113.

--- app/daos/HomeDao.rb
+++ app/daos/HomeDao.rb
@@ -1,15 +1,23 @@
 class HomeDao
     
     def initialize()
-         config()
+        config()
     end
         
     def config() 
         if (ENV['TYPE_SERVICE'] == 'client') then
             @statusServer = false
+            @socketUDP = UDPSocket.new
+            @socketUDP.bind(ENV['CLIENT_HOST'], ENV['CLIENT_PORT'])
+            messageBroadcast()
         else
             @statusServer = true
+            messageBroadcast()
         end
+    end
+        
+    def messageBroadcast()
+        
     end
     
 end
